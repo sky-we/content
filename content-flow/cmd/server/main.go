6c771826a09bfaa850efae632c6241a749d386e8
sky-we <1429670276@qq.com>
2024-10-10 16:56:05 +0800

    feat:add worker and server

---
+++ content-flow/cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"content-flow/internal/config"
+	"content-flow/internal/middleware"
+	"os"
+	"os/signal"
+)
+
+func init() {
+	config.LoadFlowCfg()
+}
+
+func main() {
+	middleware.InitLogger()
+	Logger := middleware.GetLogger()
+	fs := config.NewFlowService(config.ClientCfg.FlowService)
+	if err := fs.StartServer(); err != nil {
+		Logger.Error("go-flow service start error")
+		panic(err)
+	}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	Logger.Info("shutting down go-flow server")
+}
--- content-flow/cmd/worker/main.go
+++ content-flow/cmd/worker/main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"content-flow/internal/config"
 	"content-flow/internal/middleware"
+	"os"
+	"os/signal"
 )
 
 func init() {
 	config.LoadFlowCfg()
 }
+
 func main() {
 	middleware.InitLogger()
 	Logger := middleware.GetLogger()
 	fs := config.NewFlowService(config.ClientCfg.FlowService)
-	if err := fs.Start(); err != nil {
-		Logger.Error("go-flow service start error")
+	if err := fs.StartWorker(); err != nil {
+		Logger.Error("go-flow service start worker error")
 		panic(err)
 	}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	Logger.Info("shutting down go-flow worker")
 }
